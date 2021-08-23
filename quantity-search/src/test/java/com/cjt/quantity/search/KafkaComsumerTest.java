9eec1e9fdd1f045c945ba55d7bee97f25ae297d3
githubCJT <pkjin1013@gmail.com>
2021-08-23 23:56:37 +0800

    Update KafkaComsumerTest.java

--- quantity-search/src/test/java/com/cjt/quantity/search/KafkaComsumerTest.java
+++ quantity-search/src/test/java/com/cjt/quantity/search/KafkaComsumerTest.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 
 public class KafkaComsumerTest {
     public static void main(String[] args) {
-
         Logger logger = LoggerFactory.getLogger(KafkaComsumerTest.class);
     }
 }
