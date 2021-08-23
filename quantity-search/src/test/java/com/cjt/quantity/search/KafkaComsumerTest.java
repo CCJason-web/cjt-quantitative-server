898f3a8818da1ff1ae7d169afd82b13c45378aaa
githubCJT <pkjin1013@gmail.com>
2021-08-23 23:54:02 +0800

    Add kafka dependency

--- quantity-search/src/test/java/com/cjt/quantity/search/KafkaComsumerTest.java
+++ quantity-search/src/test/java/com/cjt/quantity/search/KafkaComsumerTest.java
@@ -7,7 +7,5 @@ import org.slf4j.LoggerFactory;
 public class KafkaComsumerTest {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(KafkaComsumerTest.class);
-
-        
     }
 }
